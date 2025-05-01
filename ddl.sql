e2e74bf381fc0fc5afcda2b05e6efc6aab33de5c
Vann06 <cas23201@uvg.edu.gt>
2025-04-30 18:37:10 -0600

    DDL Inicial

---
+++ ddl.sql
@@ -0,0 +1,47 @@
+CREATE TABLE tenants (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR
+);
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    tenant_id INTEGER REFERENCES tenants(id)
+);
+
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion TEXT NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    cantidad_disponible INTEGER NOT NULL,
+    tenant_id INTEGER NOT NULL REFERENCES tenants(id)
+);
+
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY,
+    usuario_id INTEGER REFERENCES usuarios(id),
+    tenant_id INTEGER REFERENCES tenants(id),
+    fecha TIMESTAMP NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    total DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE detallesPedidos (
+    id SERIAL PRIMARY KEY,
+    id_pedido INTEGER NOT NULL REFERENCES pedidos(id),
+    id_producto INTEGER NOT NULL REFERENCES productos(id),
+    cantidad INTEGER NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    subtotal DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE pagos (
+    id SERIAL PRIMARY KEY,
+    id_pedido INTEGER NOT NULL REFERENCES pedidos(id),
+    monto DECIMAL(10,2) NOT NULL,
+    fecha_pago TIMESTAMP NOT NULL
+);
